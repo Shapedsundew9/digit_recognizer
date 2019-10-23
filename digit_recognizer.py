fabd23e45e49151990c0338a9201f5d4d4898f91
Shaped Sundew <Shapedsundew9@users.noreply.github.com>
2019-10-23 17:31:35 -0400

    Latest best MLP

--- digit_recognizer.py
+++ digit_recognizer.py
@@ -1,20 +1,34 @@
+# Annoying warnings
+from warnings import simplefilter
+simplefilter(action='ignore', category=FutureWarning)
+
 # Make everything deterministic
 from numpy.random import seed
 seed(1)
 from tensorflow import set_random_seed
 set_random_seed(2)
 
+# More annoying warnings
+import tensorflow as tf
+tf.compat.v1.logging.set_verbosity(tf.compat.v1.logging.ERROR)
+
 import os
+import json
 import keras
 import random
 import numpy as np
 import pandas as pd
+import keras.backend as K
 import matplotlib.pyplot as plt
-from keras.models import Sequential, load_model
-from keras.layers import Dense, Dropout
+from tqdm import trange
+from keras import losses
+from keras.models import Sequential, Model, load_model
+from keras.layers import Dense, Dropout, Input, Reshape, MaxPooling1D, Concatenate, Lambda
 from keras.optimizers import RMSprop, Adam
 from keras.layers.advanced_activations import LeakyReLU
+from keras.callbacks import EarlyStopping
 from sklearn.metrics import classification_report, confusion_matrix
+from itertools import combinations
 
 
 # Training log
@@ -38,6 +52,7 @@ from sklearn.metrics import classification_report, confusion_matrix
 #    4    |  B3   |   20   |    97.86%    |  97.14%  |      N/A       |
 #    4    |  C1   |   20   |    98.43%    |  97.48%  |      N/A       |
 #    3    |  C1   |   20   |    98.45%    |  97.51%  |     97.53%     |
+#    5    |  C1   |   50   |    99.28%    |  98.19%  |     98.19%     |
 
 
 # Kaggle MNIST training and test (submission) sets
@@ -63,13 +78,12 @@ def dataset1(ratio=2.0/3.0):
 
 
 def dataset2(ratio=2.0/3.0):
-    (x_train, y_train), (x_test, y_test), x_submission = dataset1()
+    (x_train, y_train), (x_test, y_test), x_submission = dataset1(ratio)
     return (x_train / 255, y_train), (x_test / 255, y_test), x_submission / 255
 
 
 def dataset3(ratio=0.99):
-    (x_train, y_train), (x_test, y_test), x_submission = dataset2(ratio)
-    return (x_train, y_train), (x_test, y_test), x_submission
+    return dataset2(ratio)
 
 
 def dataset4(ratio=2.0/3.0):
@@ -97,6 +111,10 @@ def dataset4(ratio=2.0/3.0):
     y_test = keras.utils.to_categorical(y_test, 10)
     print("Training shape: ", x_train.shape, " Test shape: ", x_test.shape)
     return (x_train / 255, y_train), (x_test / 255, y_test), x_submission / 255
+
+
+def dataset5(ratio=0.9):
+    return dataset4(ratio)
 
 
 def modelA(dropout):
@@ -192,18 +210,20 @@ def modelC1(dropout=0.525):
               metrics=['accuracy'])
     return model
 
-
+          
 batch_size = 128
 num_classes = 10
-epochs = 20
+epochs = 500
 
-(x_train, y_train), (x_test, y_test), x_submission = dataset3()
+(x_train, y_train), (x_test, y_test), x_submission = dataset5()
+earlystop = EarlyStopping(monitor='val_acc', patience=20, verbose=1, restore_best_weights=True)
 
 if not os.path.exists('mnist.h5'):
     model = modelC1()
     history = model.fit(x_train, y_train,
                         batch_size=batch_size,
                         epochs=epochs,
+                        callbacks=[earlystop],
                         verbose=1,
                         validation_data=(x_test, y_test))
     score = model.evaluate(x_test, y_test, verbose=0)
@@ -212,6 +232,10 @@ if not os.path.exists('mnist.h5'):
     model.save('mnist.h5')
 
 model = load_model('mnist.h5')
+y_pred = model.predict(x_test, verbose=1)
+print(confusion_matrix(y_test.argmax(axis=1), y_pred.argmax(axis=1)))
+print(classification_report(y_test.argmax(axis=1), y_pred.argmax(axis=1), digits=4))
+
 
 # predict results
 results = model.predict(x_submission, verbose=1)
